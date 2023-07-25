dd2cf9a708aadc1023a332a2a596c9afc50852ae
ilya segal <65099651+ilyaVenom@users.noreply.github.com>
2023-07-25 14:52:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GuessThatNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isCorrectChoice = false;
+            Random random = new Random();
+
+            int randomNum = random.Next(1, 11);
+
+            Console.WriteLine("Welcome to the number guessing game.");
+            Console.WriteLine("A number between 1 and 10 will be generated");
+            Console.WriteLine("If you guess right, then you win");
+
+            while (!isCorrectChoice)
+            {
+                Console.WriteLine("Please enter your guess: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > randomNum)
+                {
+                    Console.WriteLine("Your guess is too hi.");
+                }
+                else if(guess < randomNum)
+                {
+                    Console.WriteLine("Guess too low");
+
+                }
+                else
+                {
+                    Console.WriteLine("right on!");
+                    isCorrectChoice = true;
+                }
+            }
+            Console.WriteLine("You won!!!");
+            Console.ReadKey();
+
+        }
+    }
+}
